fe0a21ed076ffbe83e200074fcbdb2e0406aa53b
Samariddin <gotenks.ssj499@gmail.com>
2025-12-22 14:44:37 -0500

    creted the number generator that makes a4 digit number without repeating a digit

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+import com.projectpin.console.NumberGenerator;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(NumberGenerator.generateNumber());
+    }
+}
---
+++ src/main/java/com/projectpin/console/NumberGenerator.java
@@ -0,0 +1,24 @@
+package com.projectpin.console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class NumberGenerator {
+
+    public static String generateNumber(){
+//        String[] digits={"0","1","2","3","4","5","6","7","8","9"};
+        ArrayList<String> digits = new ArrayList<>(Arrays.asList("0","1","2","3","4","5","6","7","8","9"));
+        String number="";
+        Random random=new Random();
+        for(int i=0;i<4;i++){
+            int randomDigit= random.nextInt(digits.size());
+            number=number.concat(digits.get(randomDigit));
+            digits.remove(randomDigit);
+
+        }
+
+        return number;
+    }
+
+}
